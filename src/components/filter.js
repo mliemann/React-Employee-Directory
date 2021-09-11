0b0788e25713fda579ad248264aa979092db7085
Marlon Liemann <awesomemarlon@gmail.com>
2021-09-11 16:32:18 -0400

    sort sorted (a pun :) )

--- src/components/filter.js
+++ src/components/filter.js
@@ -5,20 +5,19 @@ function Filter(props) {
     <form>
       <div className="form-group">
         <label for="Genderfilter">Filter By Gender</label>
-        <select default ="Select One" className="form-control" id="Genderfilter" onChange={event=>props.FilterG(event)}>
-          
-        <option value= {props.value3}>Select One</option>
+        <select default ="Select One" className="form-control" id="Genderfilter" onChange={event=>props.FilterG(event)}>   
+        <option value= {props.value3}></option>
         <option value= {props.value1}>Male</option>
         <option value= {props.value2}>Female</option>
           
         </select>
       </div>
       <div className="form-group">
-        <label for="Sort">Sort By Age</label>
+        <label for="Sort">Sort?</label>
         <select className="form-control" id="Sort" onChange={event=>props.Sort(event)}>
-          <option value= {props.value3}>Select One</option>
-          <option value= {props.value1}>Ascending</option>
-          <option value= {props.value2}>Decending</option>
+          <option value= {props.value3}></option>
+          <option value= {props.value1}>Age</option>
+          <option value= {props.value2}>Last</option>
         </select>
       </div>
     </form>
--- src/components/main.js
+++ src/components/main.js
@@ -18,13 +18,8 @@ class Main extends Component {
    this.setState({
     filter: value, 
 })
-   if(value ==="Male"){
-     console.log("TestMale")
-     console.log(this.state.filter)
-   } else {if(value ==="Female"){
-    console.log("TestFemale")
-  }} 
-  }
+this.filterData(event.target.value)
+  } 
 
   Sort = event => {
     console.log(event.target.value)
@@ -32,12 +27,35 @@ class Main extends Component {
    this.setState({
     sort: value, 
 })
-   if(value ==="Ascending"){
-     console.log("Ascending")
-   } else {if(value ==="Decending"){
-    console.log("Decending")
-  }}
+  this.sortData(event.target.value);
+//   if (this.state.filter===''){
+//     return;
+// } else {
+//     this.filterData(this.state.filter, this.state.results);
+// }
   }
+
+  sortData= event => {
+    console.log("function start SoRT")
+    this.state.users.sort(function(a,b) {
+        if(event==='Last') {
+            if (a.name.last > b.name.last) {
+                return 1;
+            } else if (a.name.last < b.name.last) {
+                return -1;
+            } return 0;
+        } else {if (event==='Age') {
+          if (a.dob.age > b.dob.age) {
+              return 1;
+          } else if (a.dob.age < b.dob.age) {
+              return -1;
+          }  return 0;
+      } } 
+    })
+    console.log(this.state.users);
+};
+
+
 
   componentDidMount() {
     API.fetchUsers().then((res) =>
